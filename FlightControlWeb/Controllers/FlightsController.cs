811617aee8170946a0660d6a1d9d4a6f83d88e4b
Ofir Nassimi <ofir3nassimi@gmail.com>
2020-05-24 22:14:19 +0300

    update flight location by relative_to time

--- FlightControlWeb/Controllers/FlightsController.cs
+++ FlightControlWeb/Controllers/FlightsController.cs
@@ -5,21 +5,31 @@ using Microsoft.AspNetCore.Mvc;
 using FlightControlWeb.Models;
 using FlightControlWeb.Models.Managers;
 using Newtonsoft.Json;
+using System.Drawing;
 
 namespace FlightControlWeb.Controllers
 {
+    public class Location
+    {
+        public Double Longitude { get; set; }
+        public Double Latitude { get; set; }
+    }
+
     [Route("api/[controller]")]
     [ApiController]
     public class FlightsController : ControllerBase
     {
         private FlightsManager flightsManager = new FlightsManager();
         private FlightPlansManager flightPlansManager = new FlightPlansManager();
+        private DateTime relativeToTime;
 
         // GET: api/Flights
         [HttpGet]
         public string Get(DateTime relative_to)
         {
             List<Flight> activeFlights = new List<Flight>();
+            relativeToTime = relative_to;
+
             //DateTime dateTime = TimeZoneInfo.ConvertTimeToUtc(relative_to);
             bool syncAll = Request.Query.Keys.Contains("sync_all");
 
@@ -30,6 +40,8 @@ namespace FlightControlWeb.Controllers
                     activeFlights.Add(flight);
                 }
             }
+
+            updateFlightsLocation(relative_to);
 
             return JsonConvert.SerializeObject(activeFlights);
         }
@@ -82,5 +94,83 @@ namespace FlightControlWeb.Controllers
                 return false;
             }
         }
+
+
+        private void updateFlightsLocation(DateTime relative_to)
+        {
+            foreach(Flight flight in flightsManager.GetAllFlights())
+            {
+                updateLocationBySegment(flight, flightPlansManager.GetFlightPlanById(flight.FlightId), relative_to);
+            }
+        } 
+
+
+        private void updateLocationBySegment(Flight flight, FlightPlan flightPlan, DateTime relative_to)
+        {
+            DateTime flightTime = flight.DateTime;
+            DateTime timeAfterSegment = flight.DateTime;
+            double lastLongitude = flight.Longitude;
+            double lastLatitude = flight.Latitude;
+
+            foreach(Segment segment in flightPlan.Segments)
+            {
+                timeAfterSegment = timeAfterSegment.AddSeconds(segment.TimespanSeconds);
+
+                if ((flightTime <= relative_to) && (relative_to <= timeAfterSegment))
+                {
+                    // Need to send the last time, long and lat (not the initial!!!)
+                    Location location = calculateFlightLocation(lastLongitude, lastLatitude, flightTime,
+                        timeAfterSegment, segment, relative_to);
+                    //Location location = calculateFlightLocation(flight, segment, relative_to, timeAfterSegment);
+                    break;
+                }
+                flightTime = timeAfterSegment;
+                lastLongitude = segment.Longitude;
+                lastLatitude = segment.Latitude;
+            }
+        }
+
+        private Location calculateFlightLocation(double initialLongitude, double initialLatitude,
+            DateTime currentflightTime, DateTime timeAfterSegment, Segment segment, DateTime relative_to)
+        {
+            double finalLongitude = segment.Longitude;
+            double finalLatitude = segment.Latitude;
+
+            DateTime finalTime = timeAfterSegment;
+
+            double seconds = (finalTime - currentflightTime).TotalSeconds;
+            double relativeSeconds = (relative_to - currentflightTime).TotalSeconds;
+
+            double totalDistance = Math.Sqrt(Math.Pow(finalLongitude - initialLongitude, 2) + Math.Pow(finalLatitude - initialLatitude, 2));
+            double relativeDistance = (relativeSeconds * totalDistance) / seconds;
+
+            double relativeLongitude = initialLongitude + ((finalLongitude - initialLongitude) * (relativeDistance / totalDistance));
+            double relativeLatitude = initialLatitude + ((finalLatitude - initialLatitude) * (relativeDistance / totalDistance));
+
+            return new Location { Longitude = relativeLongitude, Latitude = relativeLatitude };
+        }
+
+
+        /*private Location calculateFlightLocation(Flight flight, Segment segment, DateTime relative_to, DateTime timeAfterSegment)
+        {
+            double initialLongitude = flight.Longitude;
+            double initialLatitude = flight.Latitude;
+            double finalLongitude = segment.Longitude;
+            double finalLatitude = segment.Latitude;
+
+            DateTime initialTime = flight.DateTime;
+            DateTime finalTime = timeAfterSegment;
+
+            double seconds = (finalTime - initialTime).TotalSeconds;
+            double relativeSeconds = (relative_to - initialTime).TotalSeconds;
+            double totalDistance = Math.Sqrt(Math.Pow(finalLongitude - initialLongitude, 2) + Math.Pow(finalLatitude - initialLatitude, 2));
+
+            double relativeDistance = (relativeSeconds * totalDistance) / seconds;
+
+            double longt = initialLongitude + ((finalLongitude - initialLongitude) * (relativeDistance / totalDistance));
+            double lat = initialLatitude + ((finalLatitude - initialLatitude) * (relativeDistance / totalDistance));
+
+            return new Location { Longitude = longt, Latitude = lat };
+        }*/
     }
 }
--- FlightControlWeb/Models/Managers/FlightPlansManager.cs
+++ FlightControlWeb/Models/Managers/FlightPlansManager.cs
@@ -11,17 +11,30 @@ namespace FlightControlWeb.Models.Managers
             new FlightPlan{ FlightId = "LY319", Passengers = 217, CompanyName = "El AL",
                 InitialLocation = new InitialLocation{ Longitude = 32.08, Latitude = 34.66,
                 DateTime = DateTime.Parse("2020-05-24T09:04:00Z") },
-                Segments = new Segment[2] { new Segment { Longitude = 32.08, Latitude = 34.66, TimespanSeconds = 650 },
+                Segments = new Segment[2] { new Segment { Longitude = 32.76, Latitude = 34.92, TimespanSeconds = 650 },
                 new Segment { Longitude = 33.08, Latitude = 35.66, TimespanSeconds = 650 } } },
             new FlightPlan{ FlightId = "SWS899", Passengers = 359, CompanyName = "Swiss Air",
                 InitialLocation = new InitialLocation{ Longitude = 39.48, Latitude = -73.22,
                 DateTime = DateTime.Parse("2020-04-27T19:30:26Z") },
-                Segments = new Segment[2] { new Segment { Longitude = 39.48, Latitude = -73.22, TimespanSeconds = 760 },
-                new Segment { Longitude = 40.48, Latitude = -74.22, TimespanSeconds = 850 } } },
+                Segments = new Segment[2] { new Segment { Longitude = 40.48, Latitude = -74.22, TimespanSeconds = 760 },
+                new Segment { Longitude = 42.48, Latitude = -76.22, TimespanSeconds = 850 } } },
             new FlightPlan{ FlightId = "TK6140", Passengers = 143, CompanyName = "Turkish Airlines",
                 InitialLocation = new InitialLocation{ Longitude = 26.65, Latitude = 41.13, 
                 DateTime = DateTime.Parse("2020-04-27T19:29:26Z")},
-                Segments = new Segment[1] { new Segment { Longitude = 26.65, Latitude = 41.13, TimespanSeconds = 950 } } }
+                Segments = new Segment[1] { new Segment { Longitude = 26.95, Latitude = 41.78, TimespanSeconds = 950 } } },
+            new FlightPlan{ FlightId = "UA091", Passengers = 450, CompanyName = "United Airlines",
+                InitialLocation = new InitialLocation{ Longitude = 5, Latitude = 128, 
+                DateTime = DateTime.Parse("2020-05-24T09:40:03Z") },
+                Segments = new Segment[3] { new Segment { Longitude = 52, Latitude = 87, TimespanSeconds = 162 },
+                new Segment { Longitude = 88, Latitude = 63, TimespanSeconds = 477 },
+                new Segment { Longitude = 155, Latitude = 83, TimespanSeconds = 529 } } },
+            new FlightPlan{ FlightId = "BA413", Passengers = 330, CompanyName = "British Airways",
+                InitialLocation = new InitialLocation{ Longitude = 86, Latitude = 21, 
+                DateTime = DateTime.Parse("2020-05-24T09:35:56Z") },
+                Segments = new Segment[4] { new Segment { Longitude = 88, Latitude = 21.8, TimespanSeconds = 120 },
+                new Segment { Longitude = 88.5, Latitude = 22.6, TimespanSeconds = 986 },
+                new Segment { Longitude = 89.3, Latitude = 23, TimespanSeconds = 896 },
+                new Segment { Longitude = 90, Latitude = 23.4, TimespanSeconds = 997 } } }
         };
 
         public void AddFlightPlan(FlightPlan flightPlan)
@@ -63,5 +76,7 @@ namespace FlightControlWeb.Models.Managers
                 flightPlan.Segments = updatedFlightPlan.Segments;
             }
         }
+
+
     }
 }
