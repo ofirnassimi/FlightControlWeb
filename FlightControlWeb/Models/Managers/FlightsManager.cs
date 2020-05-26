0cc675322447d13eaa7282014132cc9ed7ab34fd
Ofir Nassimi <ofir3nassimi@gmail.com>
2020-05-26 13:15:51 +0300

    markers appear on time, move correctly according to plan and removed on time

--- FlightControlWeb/Models/Managers/FlightsManager.cs
+++ FlightControlWeb/Models/Managers/FlightsManager.cs
@@ -11,13 +11,13 @@ namespace FlightControlWeb.Models.Managers
             new Flight{ FlightId = "LY319", Longitude = 32.08, Latitude = 34.66, Passengers = 217,
                 CompanyName = "El Al", DateTime = DateTime.Parse("2020-05-24T09:04:00Z"), IsExternal = true },
             new Flight{ FlightId = "SWS899", Longitude = 39.48, Latitude = -73.22, Passengers = 359,
-                CompanyName = "Swiss Air", DateTime = DateTime.Parse("2020-04-27T19:30:26Z"), IsExternal = true },
+                CompanyName = "Swiss Air", DateTime = DateTime.Parse("2020-05-24T09:04:26Z"), IsExternal = true },
             new Flight{ FlightId = "TK6140", Longitude = 26.65, Latitude = 41.13, Passengers = 143,
-                CompanyName = "Turkish Airlines", DateTime = DateTime.Parse("2020-04-27T19:29:26Z"), IsExternal = false },
+                CompanyName = "Turkish Airlines", DateTime = DateTime.Parse("2020-05-24T09:04:36Z"), IsExternal = false },
             new Flight{ FlightId = "UA091", Longitude = 5, Latitude = 72, Passengers = 450,
-                CompanyName = "United Airlines", DateTime = DateTime.Parse("2020-05-24T09:40:03Z"), IsExternal = true },
+                CompanyName = "United Airlines", DateTime = DateTime.Parse("2020-05-24T09:04:03Z"), IsExternal = true },
             new Flight{ FlightId = "BA413", Longitude = 86, Latitude = 21, Passengers = 330,
-                CompanyName = "British Airways", DateTime = DateTime.Parse("2020-05-24T09:35:56Z"), IsExternal = false }
+                CompanyName = "British Airways", DateTime = DateTime.Parse("2020-05-24T09:04:56Z"), IsExternal = false }
         };
 
         public void AddFlight(Flight flight)
