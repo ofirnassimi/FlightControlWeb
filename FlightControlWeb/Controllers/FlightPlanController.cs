175ef473a10fcbe4ad0ebee9cb99bcdce07a7f66
Ofir Nassimi <ofir3nassimi@gmail.com>
2020-06-01 17:48:46 +0300

    drag and drop is fixed

--- FlightControlWeb/Controllers/FlightPlanController.cs
+++ FlightControlWeb/Controllers/FlightPlanController.cs
@@ -5,6 +5,7 @@ using FlightControlWeb.Models;
 using FlightControlWeb.Models.Managers;
 using System.Collections.Generic;
 using System;
+using Newtonsoft.Json.Linq;
 
 namespace FlightControlWeb.Controllers
 {
@@ -28,7 +29,49 @@ namespace FlightControlWeb.Controllers
         [HttpPost]
         public string Post([FromBody] FlightPlan flightPlan)
         {
+            string x = flightPlan.ToJson();
+            JObject json = JObject.Parse(x);
+
             flightPlan.FlightId = GenerateFlightID();
+
+            InitialLocation initialLocation = json["initial_location"].ToObject<InitialLocation>();
+
+            Flight flight = new Flight()
+            {
+                FlightId = flightPlan.FlightId,
+                Longitude = initialLocation.Longitude,
+                Latitude = initialLocation.Latitude,
+                Passengers = Int32.Parse(json["passengers"].ToString()),
+                CompanyName = json["company_name"].ToString(),
+                DateTime = initialLocation.DateTime,
+                IsExternal = false
+            };
+
+            flightsManager.AddFlight(flight);
+            flightPlansManager.AddFlightPlan(flightPlan);
+
+            return flightPlan.FlightId;
+
+
+            /*string x = flight.ToJson();
+            JObject json = JObject.Parse(x);
+
+            Flight f = new Flight();
+
+            f.FlightId = json["flight_id"].ToString();
+            f.Longitude = Convert.ToDouble(json["longitude"].ToString());
+            f.Latitude = Convert.ToDouble(json["latitude"].ToString());
+            f.Passengers = Int32.Parse(json["passengers"].ToString());
+            f.CompanyName = json["company_name"].ToString();
+            f.DateTime = DateTime.Parse(json["date_time"].ToString());
+            f.IsExternal = true;
+
+            flightsManager.AddFlight(f);
+            return f.FlightId;*/
+
+
+
+            /*flightPlan.FlightId = GenerateFlightID();
 
             Flight flight = new Flight()
             {
@@ -44,7 +87,7 @@ namespace FlightControlWeb.Controllers
             flightsManager.AddFlight(flight);
             flightPlansManager.AddFlightPlan(flightPlan);
 
-            return flightPlan.FlightId;
+            return flightPlan.FlightId;*/
         }
 
         public string GenerateFlightID()
--- FlightControlWeb/Models/Entities/FlightPlan.cs
+++ FlightControlWeb/Models/Entities/FlightPlan.cs
@@ -7,18 +7,23 @@ namespace FlightControlWeb.Models
 {
     public class FlightPlan
     {
+        [JsonPropertyName("flight_id")]
         [JsonProperty("flight_id")]
         public string FlightId { get; set; }
 
+        [JsonPropertyName("passengers")]
         [JsonProperty("passengers")]
         public int Passengers { get; set; }
 
+        [JsonPropertyName("company_name")]
         [JsonProperty("company_name")]
         public string CompanyName { get; set; }
 
+        [JsonPropertyName("initial_location")]
         [JsonProperty("initial_location")]
         public InitialLocation InitialLocation { get; set; }
 
+        [JsonPropertyName("segments")]
         [JsonProperty("segments")]
         public Segment[] Segments { get; set; }
 
